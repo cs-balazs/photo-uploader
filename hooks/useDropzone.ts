ad6821b6ca02dd6aa0ea24ff45fb4a0c81672caf
Balázs Csete <csetebalu@gmail.com>
2021-12-13 11:56:29 +0100

    separate states for progresses, hashes

--- hooks/useDropzone.ts
+++ hooks/useDropzone.ts
@@ -1,26 +1,34 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useState } from "react";
 import {
+  DropEvent,
   DropzoneOptions,
+  FileRejection,
   useDropzone as useReactDropzone,
 } from "react-dropzone";
+import { v4 as uuidv4 } from "uuid";
+
+export interface UploadedFile extends File {
+  id: string;
+  preview: string;
+}
 
 type Props = {
   maxSizeMb?: number;
   onUploadError?: (error?: Error) => void;
-} & DropzoneOptions;
-
-interface UploadedFile extends File {
-  progress: number;
-  preview: string;
-  hash?: string;
-}
+  onDrop?: (
+    acceptedFiles: Array<UploadedFile>,
+    fileRejections: FileRejection[],
+    event: DropEvent
+  ) => void;
+} & Omit<DropzoneOptions, "onDrop">;
 
 const uploadImages = async (
   files: File[],
-  clientId: number
+  clientId: string,
+  ids: string[]
 ): Promise<string[]> => {
   const formData = new FormData();
-  files.forEach((file, index) => formData.append(index.toString(), file));
+  files.forEach((file, index) => formData.append(ids[index], file));
 
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_UPLOADER_API}/${clientId}`,
@@ -41,12 +49,37 @@ const useDropzone = ({
   maxSizeMb = 10,
   ...dropzoneOptions
 }: Props = {}) => {
-  // Needed to handle SSE
-  const progressEventSource = useRef<EventSource | null>(null);
-  const uploadProgressId = useRef<number>(+Date.now());
-
-  // To store the file objects with the extra 'progress', 'preview' and 'hash' properties
+  const [progresses, setProgresses] = useState<Record<string, number>>({});
+  const [hashes, setHashes] = useState<Record<string, string>>({});
   const [acceptedFiles, setAcceptedFiles] = useState<UploadedFile[]>([]);
+
+  const setupEventSource = useCallback((clientId: string) => {
+    const source = new EventSource(
+      `${process.env.NEXT_PUBLIC_UPLOADER_API}/${clientId}`
+    );
+
+    source.addEventListener("progress", (event: Event) => {
+      try {
+        const [id, progress] = JSON.parse(
+          (event as Event & { data: string }).data
+        );
+        setProgresses((prev) => ({ ...prev, [id]: progress }));
+      } catch (error) {
+        console.error(`Failed to parse SSE "progress" event message`, error);
+      }
+    });
+
+    source.addEventListener("hash", (event: Event) => {
+      try {
+        const [id, hash] = JSON.parse((event as Event & { data: string }).data);
+        setHashes((prev) => ({ ...prev, [id]: hash }));
+      } catch (error) {
+        console.error(`Failed to parse SSE "hash" event message`, error);
+      }
+    });
+
+    return source;
+  }, []);
 
   // Start uploading files on drop event. After the POST request, the backend will start sending SSE messages
   const dropzone = useReactDropzone({
@@ -54,62 +87,46 @@ const useDropzone = ({
     accept: dropzoneOptions.accept ?? "image/*",
     noClick: dropzoneOptions.noClick ?? true,
     maxSize: dropzoneOptions.maxSize ?? maxSizeMb * 1024 * 1024,
-    onDrop: (acceptedFiles, fileRejections, event) => {
-      const newUploadedFiles = acceptedFiles.map((file) => ({
+    onDrop: (_acceptedFiles, fileRejections, event) => {
+      const uploadProgressId = uuidv4();
+      const progressEventSource = setupEventSource(uploadProgressId);
+
+      const withPreviewAndId = _acceptedFiles.map((file) => ({
         ...file,
-        progress: 0,
         preview: URL.createObjectURL(file),
+        id: uuidv4(),
       }));
 
-      setAcceptedFiles((prev) => [...prev, ...newUploadedFiles]);
+      setAcceptedFiles(withPreviewAndId);
 
-      uploadImages(acceptedFiles, uploadProgressId.current).catch(
-        onUploadError
-      );
+      setHashes((prev) => ({
+        ...prev,
+        ...Object.fromEntries(
+          Object.values(withPreviewAndId).map(({ id }) => [id, ""])
+        ),
+      }));
 
-      dropzoneOptions.onDrop?.(acceptedFiles, fileRejections, event);
+      setProgresses((prev) => ({
+        ...prev,
+        ...Object.fromEntries(
+          Object.values(withPreviewAndId).map(({ id }) => [id, 0])
+        ),
+      }));
+
+      // Intentionally not awaiting here
+      uploadImages(
+        _acceptedFiles,
+        uploadProgressId,
+        Object.values(withPreviewAndId).map(({ id }) => id)
+      )
+        .catch(onUploadError)
+        .finally(() => progressEventSource.close());
+
+      dropzoneOptions.onDrop?.(withPreviewAndId, fileRejections, event);
     },
   });
 
-  // Set up SSE client on mount
-  useEffect(() => {
-    const source = new EventSource(
-      `${process.env.NEXT_PUBLIC_UPLOADER_API}/${uploadProgressId.current}`
-    );
-    source.addEventListener("progress", (event) => {
-      const [index, progress] = JSON.parse(
-        (event as Event & { data: string }).data
-      );
-
-      setAcceptedFiles((prev) => {
-        const newAcceptedFiles = [...prev];
-        newAcceptedFiles[index].progress = progress;
-        return newAcceptedFiles;
-      });
-    });
-
-    source.addEventListener("hash", (event) => {
-      const [index, hash] = JSON.parse(
-        (event as Event & { data: string }).data
-      );
-
-      setAcceptedFiles((prev) => {
-        const newAcceptedFiles = [...prev];
-        newAcceptedFiles[index].hash = hash;
-        return newAcceptedFiles;
-      });
-    });
-
-    progressEventSource.current = source;
-  }, []);
-
-  // Disconnect SSE on unmount
-  useEffect(
-    () => () => progressEventSource.current?.close(),
-    [progressEventSource]
-  );
-
-  return { ...dropzone, acceptedFiles };
+  return { ...dropzone, progresses, hashes, acceptedFiles };
 };
 
 export default useDropzone;
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,17 +8,23 @@ import useDropzone from "../hooks/useDropzone";
 const Home: NextPage = () => {
   const toast = useToast();
 
-  const { getRootProps, getInputProps, isDragActive, acceptedFiles } =
-    useDropzone({
-      onDrop: (_, fileRejections) =>
-        fileRejections.forEach((file) =>
-          toast({
-            status: "error",
-            title: "Upload failed",
-            description: `Failed to upload file ${file.file.name}`,
-          })
-        ),
-    });
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    acceptedFiles,
+    progresses,
+    hashes,
+  } = useDropzone({
+    onDrop: (_, fileRejections) =>
+      fileRejections.forEach((file) =>
+        toast({
+          status: "error",
+          title: "Upload failed",
+          description: `Failed to upload file ${file.file.name}`,
+        })
+      ),
+  });
 
   return (
     <Grid templateColumns="1fr 2fr" h="100vh" w="100vw">
@@ -29,12 +35,12 @@ const Home: NextPage = () => {
       />
 
       <Grid alignItems="center" templateColumns="repeat(4, 1fr)">
-        {Object.entries(acceptedFiles).map(([id, file]) => (
+        {acceptedFiles.map((file) => (
           <UploadedImage
-            key={id}
+            key={file.id}
             preview={file.preview}
-            progress={file.progress}
-            hash={file.hash}
+            progress={progresses[file.id]}
+            hash={hashes[file.id]}
           />
         ))}
       </Grid>
